5f2e3e804525ab1c5cd1e6f54e367f5de8d4f4e0
Usama Shahid <itisusamashahid@gmail.com>
2023-01-21 12:52:12 +0500

    add transfer extension
    add transfer extension

---
+++ ol_transfer_extension/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+
+{
+    'name': 'Transfers Extension',
+
+    "author": "Usama Shahid | Odolution",
+    'version': '0.1',
+    'category': 'Transfer Extensions',
+    'sequence': 95,
+    'summary': 'Transfer Extensions',
+    'description': "Transfer Extensions",
+    'website': '',
+    'images': [],
+    'depends': ['stock','sale',
+    ],
+    'data': [
+        
+        "views/view.xml",
+        "security/ir.model.access.csv"
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'qweb': [
+    ],
+    'license': 'LGPL-3',
+}
---
+++ ol_transfer_extension/models/model.py
@@ -0,0 +1,25 @@
+
+from odoo import models, api, fields, _
+from odoo.exceptions import UserError
+import json
+import datetime
+
+class transfer_extension(models.Model):
+
+    _inherit = "stock.picking"
+    order_date = fields.Datetime(string='Order Date')
+    def create(self,val):
+        res = super(transfer_extension, self).create(val)
+        for rec in res:
+            sale_order=self.env['sale.order'].search([('name','=',rec.origin)])
+            if sale_order:
+                res.order_date=sale_order.date_order
+class sale_order_extension(models.Model):
+    _inherit = "sale.order"
+    @api.onchange('date_order')
+    def _onchange_date_order(self):
+        for rec in self:
+            transfers=self.env['stock.picking'].search([('origin','=',rec.name)])
+            for transfer in transfers:
+                transfer.order.date=rec.date_order
+                
